bcdd81c0585e0236f48f97956be8d48e14cdc1dc
Abigail Portuito <abigailportuito@gmail.com>
2021-10-02 20:18:05 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var css = document.querySelector("h3");
+var color1 =  document.querySelector(".color-1");
+var color2 = document.querySelector(".color-2");
+var body = document.getElementById("gradient");
+
+// console.log(body.style.background);
+// console.log(css);
+// console.log(color1);
+// console.log(color1);
+
+function setGradient(){
+    body.style.background = 
+    "linear-gradient(to right,"
+    + color1.value 
+    + ", "
+    + color2.value 
+    +")";
+
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
